19d3aae155a389f753ee3b45206909ceccebf9a8
Tennes <45150351+Tennes@users.noreply.github.com>
2018-11-18 21:22:19 +0200

    Add files via upload

---
+++ hotel.php
@@ -0,0 +1,19 @@
+<?php
+  if ($_GET["select"] == "1") {
+    echo 'Patission 80<br>';
+	 echo '+30 210 9588759<br>';
+	  echo 'Athens AJAX<br>';
+	 
+  }
+  if ($_GET["select"] == "2") {
+   echo 'York Way 115<br>';
+	 echo '+44 58984 98867<br>';
+	  echo 'London AJAX <br>';
+  }
+  
+  if ($_GET["select"] == "3") {
+   echo 'Boulevard Montmartre 90<br>';
+	 echo '+33 86946 98997<br>';
+	  echo 'Paris AJAX<br>';
+  }
+?>
